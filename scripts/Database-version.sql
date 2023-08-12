8e072447c922275744153680f792244db425eeed
Jhosh-Christopher <shuanbrj@gmail.com>
2023-08-11 22:14:41 -0300

    comentando o código

--- scripts/Database-version.sql
+++ scripts/Database-version.sql
@@ -1,6 +1,11 @@
 # https://dbdiagram.io/d/64c44e9e02bd1c4a5ee2f213
+-- Criação do banco de dados "biblioteca" caso não exista
 CREATE DATABASE IF NOT EXISTS `biblioteca`;
-use `biblioteca`;
+
+-- Utiliza o banco de dados "biblioteca"
+USE `biblioteca`;
+
+-- Tabela para armazenar informações sobre autores de livros
 CREATE TABLE `autores` (
   `id_autores` INT PRIMARY KEY,
   `nome` VARCHAR(100) NOT NULL,
@@ -8,6 +13,7 @@ CREATE TABLE `autores` (
   `data_nascimento` DATE
 );
 
+-- Tabela para armazenar informações sobre livros
 CREATE TABLE `livros` (
   `id_livros` INT PRIMARY KEY,
   `titulo` VARCHAR(255) NOT NULL,
@@ -16,12 +22,14 @@ CREATE TABLE `livros` (
   `isbn` VARCHAR(20) UNIQUE NOT NULL
 );
 
+-- Tabela para armazenar informações sobre usuários da biblioteca
 CREATE TABLE `usuarios` (
   `id_usuarios` INT PRIMARY KEY,
   `nome` VARCHAR(100) NOT NULL,
   `email` VARCHAR(255) UNIQUE NOT NULL
 );
 
+-- Tabela para armazenar informações sobre empréstimos de livros
 CREATE TABLE `emprestimos` (
   `id_emprestimos` INT PRIMARY KEY,
   `id_usuarios` INT,
@@ -31,6 +39,7 @@ CREATE TABLE `emprestimos` (
   `devolvido` BOOLEAN DEFAULT false
 );
 
+-- Tabela para armazenar informações sobre usuários bloqueados
 CREATE TABLE `bloqueados` (
   `id_bloqueados` INT PRIMARY KEY,
   `id_usuarios` INT,
@@ -38,18 +47,24 @@ CREATE TABLE `bloqueados` (
   `data_bloqueio` DATE NOT NULL
 );
 
+-- Tabela para armazenar informações sobre prazos de devolução
 CREATE TABLE `prazos` (
   `id_prazos` INT PRIMARY KEY,
   `id_emprestimos` INT,
   `data_vencimento` DATE NOT NULL
 );
 
+-- Adiciona chave estrangeira na tabela "livros" referenciando a tabela "autores"
 ALTER TABLE `livros` ADD FOREIGN KEY (`id_autores`) REFERENCES `autores` (`id_autores`);
 
+-- Adiciona chave estrangeira na tabela "emprestimos" referenciando a tabela "usuarios"
 ALTER TABLE `emprestimos` ADD FOREIGN KEY (`id_usuarios`) REFERENCES `usuarios` (`id_usuarios`);
 
+-- Adiciona chave estrangeira na tabela "emprestimos" referenciando a tabela "livros"
 ALTER TABLE `emprestimos` ADD FOREIGN KEY (`id_livros`) REFERENCES `livros` (`id_livros`);
 
+-- Adiciona chave estrangeira na tabela "bloqueados" referenciando a tabela "usuarios"
 ALTER TABLE `bloqueados` ADD FOREIGN KEY (`id_usuarios`) REFERENCES `usuarios` (`id_usuarios`);
 
+-- Adiciona chave estrangeira na tabela "prazos" referenciando a tabela "emprestimos"
 ALTER TABLE `prazos` ADD FOREIGN KEY (`id_emprestimos`) REFERENCES `emprestimos` (`id_emprestimos`);
