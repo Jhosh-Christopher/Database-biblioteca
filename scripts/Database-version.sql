887c07e5f0a124ef138f15f2d3e2be55aeae6548
Jhosh-Christopher <shuanbrj@gmail.com>
2023-08-11 21:59:50 -0300

    Ajustes dos FKs e da coesão das tabelas

--- scripts/Database-version.sql
+++ scripts/Database-version.sql
@@ -2,54 +2,54 @@
 CREATE DATABASE IF NOT EXISTS `biblioteca`;
 use `biblioteca`;
 CREATE TABLE `autores` (
-  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `id_autores` INT PRIMARY KEY,
   `nome` VARCHAR(100) NOT NULL,
   `nacionalidade` VARCHAR(100),
   `data_nascimento` DATE
 );
 
 CREATE TABLE `livros` (
-  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `id_livros` INT PRIMARY KEY,
   `titulo` VARCHAR(255) NOT NULL,
-  `id_autor` INT,
+  `id_autores` INT,
   `ano_publicacao` INT NOT NULL,
   `isbn` VARCHAR(20) UNIQUE NOT NULL
 );
 
 CREATE TABLE `usuarios` (
-  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `id_usuarios` INT PRIMARY KEY,
   `nome` VARCHAR(100) NOT NULL,
   `email` VARCHAR(255) UNIQUE NOT NULL
 );
 
 CREATE TABLE `emprestimos` (
-  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `id_emprestimos` INT PRIMARY KEY,
   `id_usuarios` INT,
-  `id_livro` INT,
+  `id_livros` INT,
   `data_emprestimo` DATE NOT NULL,
   `data_devolucao` DATE,
   `devolvido` BOOLEAN DEFAULT false
 );
 
-CREATE TABLE `bloqueadosemprestimos` (
-  `id` INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE `bloqueados` (
+  `id_bloqueados` INT PRIMARY KEY,
   `id_usuarios` INT,
   `motivo` VARCHAR(255) NOT NULL,
   `data_bloqueio` DATE NOT NULL
 );
 
 CREATE TABLE `prazos` (
-  `id` INT PRIMARY KEY AUTO_INCREMENT,
-  `id_emprestimo` INT,
+  `id_prazos` INT PRIMARY KEY,
+  `id_emprestimos` INT,
   `data_vencimento` DATE NOT NULL
 );
 
-ALTER TABLE `livros` ADD FOREIGN KEY (`id_autor`) REFERENCES `autores` (`id`);
+ALTER TABLE `livros` ADD FOREIGN KEY (`id_autores`) REFERENCES `autores` (`id_autores`);
 
-ALTER TABLE `emprestimos` ADD FOREIGN KEY (`id_usuarios`) REFERENCES `usuarios` (`id`);
+ALTER TABLE `emprestimos` ADD FOREIGN KEY (`id_usuarios`) REFERENCES `usuarios` (`id_usuarios`);
 
-ALTER TABLE `emprestimos` ADD FOREIGN KEY (`id_livro`) REFERENCES `livros` (`id`);
+ALTER TABLE `emprestimos` ADD FOREIGN KEY (`id_livros`) REFERENCES `livros` (`id_livros`);
 
-ALTER TABLE `bloqueados` ADD FOREIGN KEY (`id_usuarios`) REFERENCES `usuarios` (`id`);
+ALTER TABLE `bloqueados` ADD FOREIGN KEY (`id_usuarios`) REFERENCES `usuarios` (`id_usuarios`);
 
-ALTER TABLE `prazos` ADD FOREIGN KEY (`id_emprestimo`) REFERENCES `emprestimos` (`id`);
+ALTER TABLE `prazos` ADD FOREIGN KEY (`id_emprestimos`) REFERENCES `emprestimos` (`id_emprestimos`);
